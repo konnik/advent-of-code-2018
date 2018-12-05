4a91da5e8cb773439e447944a613b60a0d39858d
konnik <niklas.konstenius@gmail.com>
2018-12-05 19:17:08 +0100

    Day 5

--- src/Util.elm
+++ src/Util.elm
@@ -2,3 +2,9 @@ module Util exposing (..)
 
 int : String -> Int
 int str = String.toInt str |> Maybe.withDefault 0  
+
+minimum : List number -> number
+minimum list = 
+    case (List.minimum list) of
+        Just value -> value
+        Nothing -> Debug.todo "should not happen" 
